d98d69c45e269ca6dfb83f71c6ff1035f4b0f9bc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 20:50:57 -0300

    Fazer em casa

--- Apostila03/src/br/com/fiap/loja/view/Terminal.java
+++ Apostila03/src/br/com/fiap/loja/view/Terminal.java
@@ -2,6 +2,10 @@ package br.com.fiap.loja.view;
 
 public class Terminal {
 
-
+    //criar main
+    //ler os dados do produto
+    //Criar o objeto produto
+    //Atribuir os valores ao objeto
+    //Exibir os valores
 
 }
