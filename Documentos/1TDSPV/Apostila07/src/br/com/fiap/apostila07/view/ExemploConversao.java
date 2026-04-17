8b0e140c22d11eb44e20cb454aec909e26f78f8a
KaiqueZiantoni <kaiquezrosa@gmail.com>
2026-04-17 20:56:50 -0300

    Apostila exercicio 07 (inicio) Adicionado

--- Documentos/1TDSPV/Apostila07/src/br/com/fiap/apostila07/view/ExemploConversao.java
+++ Documentos/1TDSPV/Apostila07/src/br/com/fiap/apostila07/view/ExemploConversao.java
@@ -1,8 +1,17 @@
 package br.com.fiap.apostila07.view;
 
+import javax.swing.*;
+
 public class ExemploConversao {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
+        String numero1 = JOptionPane.showInputDialog("Digite um numero");
+        int n1 = Integer.parseInt(numero1);
+
+        String numero2 = JOptionPane.showInputDialog("Digite o segundo numero: ");
+        int n2 = Integer.parseInt(numero2);
+
+        JOptionPane.showMessageDialog(null, "A soma é : "  +  (n1+n2));
     }
 }
